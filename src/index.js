4efbf266aa00812c51b9e39a77898c581a73685b
eugene23021997 <eugene.couybes@gmail.com>
2025-05-07 16:12:29 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
-import React from "react";
+import * as React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-// Modification du chemin d'import pour les styles
 import "./styles/main.css";
 
-/**
- * Point d'entrée de l'application
- * Rend le composant App dans l'élément racine
- */
 ReactDOM.render(
   <React.StrictMode>
     <App />
